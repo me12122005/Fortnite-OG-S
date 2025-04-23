b5e7e4ffea2ffc3c896e9b57d7eb6bedf7c18718
El Handouz Almoustafa (s150466) <almoustafa.elhandouz@student.ap.be>
2025-04-23 13:40:26 +0000

    get requests

--- app.ts
+++ app.ts
@@ -6,12 +6,30 @@ app.set("port", 3000);
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.get("/",(req,res)=>{
-    res.render("detail");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/game",(req,res)=>{
-    res.render("game");
+
+app.get("/favorite", (req, res) => {
+  res.render("favorite");
+});
+
+
+app.get("/game", (req, res) => {
+  res.render("game");
+});
+
+app.get("/library", (req, res) => {
+  res.render("library");
+});
+
+app.get("/blacklist", (req, res) => {
+  res.render("blacklist");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(app.get("port"), () =>
